7736bf8cc660728bb7b2eb01856f4aac052945f1
MaxMorgon <maxmorgon@gmail.com>
2025-05-21 18:15:34 +0200

    Added ability to create components on game objects using the scene creator

--- Editor/Creator/SceneCreatorWindow.cs
+++ Editor/Creator/SceneCreatorWindow.cs
@@ -12,8 +12,9 @@ namespace Editor.Creator
         private const string WINDOW_HEADER = "Create in Scene";
 
         private static List<string> menuItemsCache;
-        private static Dictionary<string, GameObject> componentsCache;
+        private static Dictionary<string, Type> componentsTypeByKey;
         private static Dictionary<string, GameObject> prefabsByItem;
+        private static List<string> allItems;
         
         [MenuItem("Tools/Batnip/Scene Creator")]
         private static void OpenSceneCreator()
@@ -25,21 +26,49 @@ namespace Editor.Creator
         
         protected override List<string> FetchItems()
         {
-            var result = new List<string>();
+            allItems ??= new List<string>();
+            allItems.Clear();
+            
+            // Initialize compile time related keys
             if (menuItemsCache == null)
             {
                 menuItemsCache = new List<string>();
                 var assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
                 var types = assemblies
-                    .SelectMany(asm => asm.GetTypes());
+                    .SelectMany(asm => asm.GetTypes())
+                    .ToArray();
             
                 PopulateMenuItems(types, MENU_ITEMS_FOLDER, menuItemsCache);
+                PopulateComponents(types);
             }
             
-            result.AddRange(menuItemsCache);
-            PopulatePrefabs(result);
-            return result;
+            // Add cached to items
+            allItems.AddRange(menuItemsCache);
+            allItems.AddRange(componentsTypeByKey.Keys);
+            
+            // Populate prefabs
+            PopulatePrefabs(allItems);
+            return allItems;
+        }
+
+        private void PopulateComponents(Type[] types)
+        {
+            var componentTypes = types.Where(IsInstantiatableComponent);
+            componentsTypeByKey = new Dictionary<string, Type>();
+            foreach (var componentType in componentTypes)
+            {
+                if (!componentsTypeByKey.TryAdd(componentType.FullName, componentType))
+                {
+                    var other = componentsTypeByKey[componentType.FullName!];
+                    Debug.Log($"Found duplicate component of type {componentType.Name} fullname1: {componentType.FullName} fullname2: {other.FullName}");
+                }
+            }
+        }
+        
+        private bool IsInstantiatableComponent(Type type){
+            return type.IsSubclassOf(typeof(Component))
+                && !type.IsAbstract;
         }
 
         private void PopulatePrefabs(List<string> populateInto)
@@ -58,6 +87,19 @@ namespace Editor.Creator
 
         protected override void ItemRunRequested(string item)
         {
+            if (componentsTypeByKey.TryGetValue(item, out var componentType))
+            {
+                var selectedGameObject = Selection.activeGameObject;
+                if (selectedGameObject == null)
+                {
+                    Debug.LogError($"Can't add component {componentType.Name} as there is no game object selected.");
+                    return;
+                }
+                
+                selectedGameObject.AddComponent(componentType);
+                return;
+            }
+            
             if (prefabsByItem.TryGetValue(item, out var prefab))
             {
                 var selectedGameObjectTransform = Selection.activeGameObject?.transform;
